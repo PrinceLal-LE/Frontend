b32d546b2cdc84d6e4e2d825890ce892cc9b5d92
prince-zurato <prince@zuratotechnologies.com>
2025-07-10 00:33:35 +0530

    My profile complete

--- src/views/profile.jsx
+++ src/views/profile.jsx
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap"
 import React from "react";
 import { MyProfile } from "./Profile/MyProfile";
 import Advertisment from './Home/Components/Advertisment'
+import TrendingContent from './SkillConnect/Components/TrendingContent'
 export const Profile = () => {
     return (
         <>
@@ -10,15 +11,15 @@ export const Profile = () => {
 
                     {/* Main content area */}
                     <Col md={9}>
-                        {/* Search Pane */}
-                        <MyProfile/>
-                        {/* Left content area */}
+                        <MyProfile />
                     </Col>
 
                     {/* Right content area */}
                     <Col md={3}>
                         {/* Right content goes here */}
-                        <Advertisment/>
+                        <Advertisment />
+                        <div className="mb-2"></div>
+                        <TrendingContent />
                     </Col>
                 </Row>
             </Container>
